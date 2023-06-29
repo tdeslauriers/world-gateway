d359d231c5a7023c0788c0c47df61b7c3e460db8
tdeslauriers <thomas.deslauriers@gmail.com>
2023-06-29 05:04:14 -0500

    add description field

--- src/main/java/world/deslauriers/model/gallery/BackupAlbum.java
+++ src/main/java/world/deslauriers/model/gallery/BackupAlbum.java
@@ -1,10 +1,15 @@
 package world.deslauriers.model.gallery;
 
+import io.micronaut.core.annotation.Nullable;
 import io.micronaut.serde.annotation.Serdeable;
 
 @Serdeable
 public record BackupAlbum(
         Long id,
-        String album
+        String album,
+        @Nullable String description
 ) {
+    public BackupAlbum(Long id, String album) {
+        this(id, album, null);
+    }
 }
